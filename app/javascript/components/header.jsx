0a1415e38278ba481a1abdbefb39d1396920386b
Elias Miranda <eliasmiranda@Eliass-MacBook-Pro.local>
2022-12-19 12:03:31 -0600

    rounded

--- app/javascript/components/header.jsx
+++ app/javascript/components/header.jsx
@@ -17,7 +17,7 @@ export default function Header() {
   }
   return (
       
-<nav className="bg-black border-gray-200 px-2 sm:px-4 py-2.5 rounded z-10 sticky top-0">
+<nav className="bg-black border-gray-200 px-2 sm:px-4 py-2.5 rounded z-10 sticky top-0 rounded-full w-full">
   <div className="bg-black container flex flex-wrap items-center justify-between mx-auto">
     <Link to="*" className="flex items-center">
         <img src={Logo} alt=" Logo" />
