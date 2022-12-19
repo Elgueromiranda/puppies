575614e3186e029682fff46b6155a4a1ce28637e
Elias Miranda <eliasmiranda@Eliass-MacBook-Pro.local>
2022-12-19 10:41:44 -0600

    pic spacing

--- app/javascript/components/pages/about.jsx
+++ app/javascript/components/pages/about.jsx
@@ -6,8 +6,11 @@ export default function About() {
 		<div  className="grid place-items-center h-screen"  style={{paddingTop:"40px",
 					paddingBottom: "300px",
 					backgroundColor: "#FFFFE0"}}>
-			<img src={certificate} /> 
-			<div className="container bg-indigo-400 text-white rounded-md">
+					<div className="p-4 bg-amber-900 rounded-md">
+			<img src={certificate} />
+								</div>
+ 
+			<div className="container bg-indigo-400 text-white rounded-md mt-3">
 			Lake in the Hills has a responsible breeding program. Our lines are guarenteed to be healthy and all dogs come with a health guarantee. We are AKC registered and have succesfully delivered our puppies to many happy customers. Our dogs are family and We want to ensure their puppies end up in a good home.
 			<br/>
 			<br/>
--- app/javascript/components/pages/pets.jsx
+++ app/javascript/components/pages/pets.jsx
@@ -14,22 +14,27 @@ export default function Pets() {
 									backgroundColor: "#FFFFE0"}}>
 
 			<div  className="h-96 carousel carousel-vertical rounded-lg">
-  <div className="carousel-item h-full">
-    <img src={Bucky} />
+  <div className="carousel-item " style={{width: "650px",
+                                          height: "400px"}}>
+    <img src={Bucky} className="object-fill w-full h-full " />
   </div> 
-  <div className="carousel-item h-full">
-    <img src={Mj} />
+  <div className="carousel-item " style={{width: "650px",
+                                          height: "400px"}}>
+    <img src={Mj} className="object-fill w-full h-full " />
   </div> 
-  <div className="carousel-item h-full">
-    <img src={Pepper} />
+  <div className="carousel-item " style={{width: "650px",
+                                          height: "400px"}}>
+    <img src={Pepper} className="object-fill w-full h-full"/>
   </div> 
 </div>
 			<div className="h-96 carousel carousel-vertical rounded-lg mt-4">
-  <div className="carousel-item h-full">
-    <img src={Stark} />
+  <div className="carousel-item " style={{width: "650px",
+                                          height: "400px"}}>
+    <img src={Stark} className="object-fill w-full h-full " />
   </div> 
-  <div className="carousel-item h-full">
-    <img src={Thor} />
+  <div className="carousel-item " style={{width: "650px",
+                                          height: "400px"}}>
+    <img src={Thor} className="object-fill  w-full h-full" />
   </div> 
 
   
