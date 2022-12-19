8f50adb88475795d2452ffda7294c73a791228ee
Elias Miranda <eliasmiranda@Eliass-MacBook-Pro.local>
2022-12-19 11:26:21 -0600

    fix

--- app/javascript/components/pages/pets.jsx
+++ app/javascript/components/pages/pets.jsx
@@ -8,33 +8,33 @@ import Thor from '../../images/puppies/thor.jpg'
 
 export default function Pets() {
 	return (
-	<div className="grid place-items-center h-screen" style={{paddingTop: "40px",
+	<div className="grid place-items-center h-screen " style={{paddingTop: "50px",
 									paddingLeft: "30px",
-									paddingBottom: "190px",
+									paddingBottom: "900px",
 									backgroundColor: "#FFFFE0"}}>
 
-			<div  className="h-96 carousel carousel-vertical rounded-lg">
-  <div className="carousel-item " style={{width: "650px",
+			<div  className="h-96 carousel carousel-vertical rounded-lg shrink">
+  <div className="carousel-item shrink " style={{width: "650px",
                                           height: "400px"}}>
-    <img src={Bucky} className="object-fill w-full h-full " />
+    <img src={Bucky} className="object-fill w-full h-full shrink " />
   </div> 
-  <div className="carousel-item " style={{width: "650px",
+  <div className="carousel-item shrink " style={{width: "650px",
                                           height: "400px"}}>
-    <img src={Mj} className="object-fill w-full h-full " />
+    <img src={Mj} className="object-fill w-full h-full shrink " />
   </div> 
-  <div className="carousel-item " style={{width: "650px",
+  <div className="carousel-item shrink " style={{width: "650px",
                                           height: "400px"}}>
-    <img src={Pepper} className="object-fill w-full h-full"/>
+    <img src={Pepper} className="object-fill w-full h-full shrink"/>
   </div> 
 </div>
-			<div className="h-96 carousel carousel-vertical rounded-lg mt-4">
-  <div className="carousel-item " style={{width: "650px",
+			<div className="h-96 carousel carousel-vertical rounded-lg mt-7 shrink">
+  <div className="carousel-item shrink " style={{width: "650px",
                                           height: "400px"}}>
-    <img src={Stark} className="object-fill w-full h-full " />
+    <img src={Stark} className="object-fill w-full h-full shrink " />
   </div> 
-  <div className="carousel-item " style={{width: "650px",
+  <div className="carousel-item shrink " style={{width: "650px",
                                           height: "400px"}}>
-    <img src={Thor} className="object-fill  w-full h-full" />
+    <img src={Thor} className="object-fill  w-full h-full shrink" />
   </div> 
 
   
