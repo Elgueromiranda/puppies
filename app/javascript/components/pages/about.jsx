a5611ef67598d17c913d7216e2f4c09a1c3e047e
Elias Miranda <eliasmiranda@Eliass-MacBook-Pro.local>
2022-12-19 16:21:30 -0600

    more f

--- app/javascript/components/pages/about.jsx
+++ app/javascript/components/pages/about.jsx
@@ -6,21 +6,19 @@ export default function About() {
 		<div  className="grid place-items-center h-screen"  style={{paddingTop:"40px",
 					paddingBottom: "300px",
 					backgroundColor: "#FFFFE0"}}>
-					<div className="grid grid-cols-1">
 					<div className="p-4 bg-amber-900 mr-2 ml-2 margin-auto flex">
 			<img src={certificate} />
-				<div className="bg-white text-red-600 p-4 ">
+								
+				</div>
+				<div className="bg-red-600 text-white p-4 mt-5 container">
 				<p className="font-serif text-md">
 
-				All puppies will be accompanied by a flight nanny. We will not crate ship.
+				All puppies will be accompanied by a flight nanny. We will not crate ship!
 				Pick ups are also availble. We will meet you half way for delivery if your
 				are within driving distance Otherwise, you will have to pay for delivery
 				</p>
 				</div>
-								
-				</div>
 
-		</div>
  
 			<div className="container bg-indigo-400 text-white rounded-md mt-3 p-3">
 			Lake in the Hills has a responsible breeding program. Our lines are guarenteed to be healthy and all dogs come with a health guarantee. We are AKC registered and have succesfully delivered our puppies to many happy customers. Our dogs are family and We want to ensure their puppies end up in a good home.
