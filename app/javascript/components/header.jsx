38d7d4527ffbcfee1a61f4737124232c55b044f6
Elias Miranda <eliasmiranda@Eliass-MacBook-Pro.local>
2022-12-27 16:59:51 -0600

    added email js

--- app/javascript/components/header.jsx
+++ app/javascript/components/header.jsx
@@ -29,16 +29,16 @@ export default function Header() {
     <div className={menu ? " w-full md:block md:w-auto" : "hidden w-full md:block md:w-auto"} id="navbar-default">
       <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          <Link to="*" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</Link>
+          <Link to="*" className="block font-serif py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</Link>
         </li>
         <li>
-          <Link to="/about" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</Link>
+          <Link to="/about" className="block font-serif py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</Link>
         </li>
         <li>
-          <Link to="/pets" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Availble Pets</Link>
+          <Link to="/pets" className="block font-serif py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Availble Pets</Link>
         </li>
         <li>
-          <Link to="/application" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Application</Link>
+          <Link to="/application" className="block font-serif py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Application</Link>
         </li>
    
       </ul>
--- app/javascript/components/pages/about.jsx
+++ app/javascript/components/pages/about.jsx
@@ -1,31 +1,49 @@
 import React from 'react'
 import certificate from '../../images/certificate.jpeg'
+import gooddog from '../../images/logos/gooddog.jpg'
+
 
 export default function About() {
 	return (
-		<div  className="grid place-items-center h-screen"  style={{paddingTop:"40px",
-					paddingBottom: "300px",
+		<div className="place-content-center" style={{paddingTop:"40px",
+					paddingBottom: "500px",
 					backgroundColor: "#FFFFE0"}}>
-					<div className="p-4 bg-amber-900 mr-2 ml-2 margin-auto flex">
-			<img src={certificate} />
-								
-				</div>
-				<div className="bg-red-600 text-white p-4 mt-5 container">
-				<p className="font-serif text-md">
+
+
+<div className="grid grid-cols-2 container place-items-center">
+  <div className="ml-5 mb-5 mr-1">
+  <img src={certificate} className="border-8 rounded border-amber-900" />
+  </div>
+    <div className="mr-2 mb-5 ml-1">
+
+  <img src={gooddog} className="object-scale-down h-70 w-80"/>	
+    </div>
+			
+</div>
+<div class="grid grid-col-2 ml-5 mr-3">
+			<div>		<p className="font-serif bg-red-600 container text-white rounded p-3">
 
 				All puppies will be accompanied by a flight nanny. We will not crate ship!
 				Pick ups are also availble. We will meet you half way for delivery if your
-				are within driving distance Otherwise, you will have to pay for delivery
+				are within driving distance Otherwise, you will have to pay for delivery.
 				</p>
-				</div>
 
- 
-			<div className="container bg-indigo-400 text-white rounded-md mt-3 p-3">
+
+
+</div>
+		
+	<div className="container bg-indigo-400 text-white rounded-md mt-3 p-3">
 			Lake in the Hills has a responsible breeding program. Our lines are guaranteed to be healthy and all dogs come with a health guarantee. We are AKC registered and have succesfully delivered our puppies to many happy customers. Our dogs are family and We want to ensure their puppies end up in a good home.
 			<br/>
 			<br/>
 			 If you would like to purchase a puppy you need to fill out an application.
 			</div>
-		</div>
+	
+		
+	</div>
+
+</div>
+
+	
 	)
 }
